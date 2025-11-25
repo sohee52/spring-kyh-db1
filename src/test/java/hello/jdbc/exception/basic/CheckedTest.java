ee34063e057fe1414b3a66db9866d817d3399b3a
sohee52 <wneh2023@naver.com>
2025-11-25 23:00:47 +0900

    5. 자바 예외 이해

--- src/test/java/hello/jdbc/exception/basic/CheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -11,12 +11,17 @@ public class CheckedTest {
     @Test
     void checked_catch() {
         Service service = new Service();
-        service.callCatch();
+        service.callCatch(); // 이 메서드가 예외를 잡아서 처리하기 때문에 테스트 메서드까지 예외가 올라오지 않는다.
+//        실행 순서
+//        1. test service.callCatch() repository.call() [예외 발생, 던짐]
+//        2. test service.callCatch() [예외 처리] repository.call()
+//        3. test [정상 흐름] service.callCatch() repository.call()
     }
 
     @Test
     void checked_throw() {
         Service service = new Service();
+        // service.callThrow() 메서드는 체크 예외를 밖으로 던지기 때문에, 테스트 메서드에서 예외를 잡아야 한다.
         assertThatThrownBy(() -> service.callThrow())
                 .isInstanceOf(MyCheckedException.class);
     }
@@ -43,8 +48,8 @@ public class CheckedTest {
         public void callCatch() {
             try {
                 repository.call();
-            } catch (MyCheckedException e) {
-                //예외 처리 로직
+            } catch (MyCheckedException e) { // 체크 예외를 잡아서 처리하려면 catch(...)를 사용해야 한다.
+                // 로그를 남길 때 로그의 마지막 인수에 예외 객체를 전달해주면 로그가 해당 예외의 스택 트레이스를 추가로 출력해준다.
                 log.info("예외 처리, message={}", e.getMessage(), e);
             }
         }
